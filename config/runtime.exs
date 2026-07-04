e1e85d1f102350b1e2b358499ac3808c3a1abaf8
bonsai <onsen.bonsai@gmail.com>
2026-07-04 14:41:45 +0900

    feat: Railway deploy support with railway.json

--- config/runtime.exs
+++ config/runtime.exs
@@ -30,11 +30,13 @@ if config_env() == :prod do
 
   maybe_ipv6 = if System.get_env("FLY_APP_NAME"), do: [:inet6], else: []
 
+  use_ssl = not (System.get_env("FLY_APP_NAME") || System.get_env("RAILWAY_SERVICE_ID"))
+
   config :voice_bbs, VoiceBbs.Repo,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6,
-    ssl: !System.get_env("FLY_APP_NAME")
+    ssl: use_ssl
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
